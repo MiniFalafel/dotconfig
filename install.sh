fd4f84b695b7037e1ddce2f8288fdaef1001729a
Eli Sanders <69729819+MiniFalafel@users.noreply.github.com>
2024-11-22 23:21:39 -0700

    Updated install scripts to use --needed flag when installing dependencies

--- install.sh
+++ install.sh
@@ -13,11 +13,11 @@ bash -c "$SCRIPT_DIR/.config/nvim/install.sh"
 
 # Install dependencies
 if test -f "$SCRIPT_DIR/pkglist.txt"; then
-    pacman -S - < "$SCRIPT_DIR/pkglist.txt"
+    pacman -S --needed - < "$SCRIPT_DIR/pkglist.txt"
 fi
 # Install AUR dependencies as non-root user
 if test -f "$SCRIPT_DIR/yay-pkglist.txt"; then
     NON_ROOT=$(who am i | awk '{print $1}')
-    sudo -u $NON_ROOT bash -c "yay -S - < $SCRIPT_DIR/yay-pkglist.txt"
+    sudo -u $NON_ROOT bash -c "yay -S --needed - < $SCRIPT_DIR/yay-pkglist.txt"
 fi
 
